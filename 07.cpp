3670164c3c83ffdd2127305dce7231408895f3ba
Anubhav Maheshwari <anubhavmaheshwari0805@gmail.com>
2021-06-28 17:25:44 +0530

    All Programs Done

--- 07.cpp
+++ 07.cpp
@@ -1,27 +1,37 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
+
 int gcd(int a,int b) {
     int t;
     if(b==0)
         return a;
     return gcd(b,a%b);
 }
+/*int modmul(int m,int e,int n) {
+    int c=m;
+    for(int i=1;i<e;i++)
+        c=fmod(c*m,n);
+    return c;
+}*/
 int modmul(int m,int e,int n) {
     int rev = 0;
+    int c=0;
     while (e > 0)
     {
         rev <<= 1;
         if (e & 1 == 1)
             rev ^= 1;
-        e >>= 1;        
+        e >>= 1;
+        c++;        
     }
     int d=1;
-    while(rev>0) {
+    while(c>0) {
         d=fmod(d*d,n);
         if(rev&1==1)
             d=fmod(d*m,n);
         rev>>=1;
+        c--;
     }
     return d;
 }
@@ -45,14 +55,8 @@ main() {
     int msg,en,dec;
     cout<<"Enter Message : ";
     cin>>msg;
-    //en=msg;
-    //for(int i=1;i<e;i++)
-    //    en=fmod(en*msg,n);
     en=modmul(msg,e,n);
     cout<<"Encrypted Message : "<<en<<"\n";
-    //dec=en;
-    //for(int i=1;i<d;i++)
-    //    dec=fmod(dec*en,n);
     dec=modmul(en,d,n);
     cout<<"Decrypted Message : "<<dec<<"\n";
 }
--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 3. Implement Data encryption and decryption using Hill Cipher method.
 4. Encrypt the plaintext we using a Vigenere table that consists of the alphabet from A to Z written out 26 times in different rows, further each alphabet must be shifted cyclically to the left compared to the previous alphabet.
 5. Implement Rail fence cipher technique using the row & Column Transformation.
-6. Demonstrate the Data Encryption Standard based on the two fundamental attributes of cryptography: substitution (also called as confusion) and transposition (also called as diffusion).
+6. Demonstrate the Simplified Data Encryption Standard based on the two fundamental attributes of cryptography: substitution (also called as confusion) and transposition (also called as diffusion).
 7. Execute the program for simple RSA algorithm to encrypt and decrypt the data.
 8. Implement diffie Hellman (DH) key exchange algorithm as a method for securely exchanging cryptographic keys over a public communications channel.
